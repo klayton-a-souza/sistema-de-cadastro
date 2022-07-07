d6d0fc58f8dd3d6049d9abbcd3c735c071fd57fb
klayton-a-souza <klayton.al.souza@gmail.com>
2022-07-07 09:46:20 -0300

    voltando para o estado do colaborador

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de mangas")
 console.log("Aplicação está se conectando ao banco de dados")
 //mais um console log de teste:
 console.log("commit durante a aula!")
-console.log("Enviando alteração para o GitHub")
