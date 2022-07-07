252fa65702b04653c6298ca32ab73eedf8b7f602
klayton-a-souza <klayton.al.souza@gmail.com>
2022-07-07 09:38:28 -0300

    adicionando um novo console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de mangas")
 console.log("Aplicação está se conectando ao banco de dados")
 //mais um console log de teste:
 console.log("commit durante a aula!")
+console.log("Enviando alteração para o GitHub")
