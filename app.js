3dc5dac1c077f6d0fc85f319b8103676cb893f82
klayton-almeida <108874470+klayton-almeida@users.noreply.github.com>
2022-07-07 09:36:06 -0300

    alteração pelo colaborador

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de mangas")
 console.log("Aplicação está se conectando ao banco de dados")
+//mais um console log de teste:
+console.log("commit durante a aula!")
